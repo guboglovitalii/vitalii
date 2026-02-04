e44eb0417bb9f74f2dfd7db93e2a0e612f5e3de5
guboglovitalii <vitaliiguboglo@gmail.com>
2026-02-04 03:22:56 +0200

    добавили проверки, корелляцию и новый сценарий

--- user_classes/wt_base_scenario.py
+++ user_classes/wt_base_scenario.py
@@ -1,23 +1,95 @@
-from locust import task, SequentialTaskSet, HttpUser, constant_pacing, events
+from locust import task, SequentialTaskSet, HttpUser, constant_pacing, events, FastHttpUser
+import sys, re, random
 from config.config import cfg, logger
-
+from utils.assertion import check_http_response
+from utils.non_test_methods import open_csv_file, open_random_csv_file
 
 class PurchaseFlightTicket(SequentialTaskSet): # класс с задачами (содержит основной сценарий)
+    def on_start(self):
+        self.user_data_csv_file = './test_data/user_data.csv'
+        self.users_data = open_csv_file(self.user_data_csv_file)
+        self.random_users_data = open_random_csv_file(self.user_data_csv_file)
+        logger.info(f"______ СПИСОК ПОЛЬЗОВАТЛЕЙ: {self.random_users_data}")
+
     @task()
     def uc_00_getHomePage(self):
-        r001_01_response = self.client.get(
+        r00_01_WebTours = self.client.get(
             '/WebTours/',
-            name="r001_01_response",
+            name="r00_01_response",
             allow_redirects=False,
             headers={
                 'sec-ch-ua': 'Not(A:Brand";v="8", "Chromium";v="144", "Microsoft Edge";v="144',
                 'sec-ch-ua-mobile': '?0'
-            } 
+            },
+            # debug_stream=sys.stderr
         )
-        logger.info(f"Статус ответа: {r001_01_response.status_code}, Тело ответа: {r001_01_response.text}")
-        print(f"Статус ответа: {r001_01_response.status_code}, Тело ответа: {r001_01_response.text}") 
 
-class WebToursBaseUserClass(HttpUser): # юзер-класс, принимающий в себя основные параметры теста
+        with self.client.get(
+            '/WebTours/header.html',
+            name="r00_02_header_html",
+            allow_redirects=False,
+            # debug_stream=sys.stderr,
+            catch_response=True
+        ) as r00_02_header_html:
+            check_http_response(r00_02_header_html, 'images/webtours.png')
+
+
+        with self.client.get(
+            '/cgi-bin/welcome.pl?signOff=true',
+            name="r00_03_welcome_pl",
+            allow_redirects=False,
+            catch_response=True
+            # debug_stream=sys.stderr
+        ) as r00_03_welcome_pl:
+            check_http_response(r00_03_welcome_pl, 'Web Tours')
+
+        with self.client.get(
+            '/cgi-bin/nav.pl?in=home',
+            name="r00_04_nav_pl",
+            allow_redirects=False,
+            catch_response=True
+            # debug_stream=sys.stderr
+        ) as r00_04_nav_pl:
+            check_http_response(r00_04_nav_pl, 'Web Tours Navigation Bar')
+
+            self.user_session = re.search(r"name=\"userSession\" value=\"(.*)\"\/", r00_04_nav_pl.text).group(1)
+
+        with self.client.get(
+            '/WebTours/home.html',
+            name="r00_05_home_html",
+            allow_redirects=False,
+            catch_response=True
+            # debug_stream=sys.stderr
+        ) as r00_05_home_html:
+            check_http_response(r00_05_home_html, 'Welcome to the Web Tours site.')
+        # logger.info(f"Статус ответа: {r001_01_response.status_code}, Тело ответа: {r001_01_response.text}")
+        # print(f"Статус ответа: {r001_01_response.status_code}, Тело ответа: {r001_01_response.text}") 
+
+    @task()
+    def uc_01_login(self):
+        self.users_row = next(self.users_data)
+        self.random_users_row = random.choice(self.random_users_data)
+        self.login = self.random_users_row["username"]
+        self.password = self.random_users_row["password"]
+
+
+        self.body_r01_login_pl = f'userSession={self.user_session}&username={self.login}&password={self.password}&login.x=73&login.y=12&JSFormSubmit=off'
+        print(f"_______BODY LOGIN: {self.body_r01_login_pl}")
+        logger.info(f"_______BODY LOGIN: {self.body_r01_login_pl}")
+        with self.client.post(
+            '/cgi-bin/login.pl',
+            name="r01_01_login",
+            allow_redirects=False,
+            catch_response=True,
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded'
+                },
+            data=self.body_r01_login_pl    
+            # debug_stream=sys.stderr
+        ) as r01_01_login:
+            check_http_response(r01_01_login, 'Web Tours')
+
+class WebToursBaseUserClass(FastHttpUser): # юзер-класс, принимающий в себя основные параметры теста
     wait_time = constant_pacing(cfg.pacing)
     host = cfg.url
     tasks = [PurchaseFlightTicket]
