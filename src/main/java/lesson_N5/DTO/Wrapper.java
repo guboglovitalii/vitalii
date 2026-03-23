e5ac90c91b6f8d6b27920c02728245302655d99b
guboglovitalii <vitaliiguboglo@gmail.com>
2026-03-23 17:34:43 +0200

    Home work N4

--- src/main/java/lesson_N5/DTO/Wrapper.java
+++ src/main/java/lesson_N5/DTO/Wrapper.java
@@ -1,14 +1,18 @@
 package lesson_N5.DTO;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Wrapper {
+    @JsonProperty("Persona1")
     private PersonaDto persona1;
+    @JsonProperty("Persona2")
     private PersonaDto persona2;
 
 }
